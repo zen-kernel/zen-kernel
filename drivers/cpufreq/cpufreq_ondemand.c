9e03ec6061e2a294f05b6ff1da37cffa020ac3e5
Steven Barrett <damentz@liquorix.net>
2012-11-03 15:53:21 -0500

    zen-tune: add custom configuration for cpufreq ondemand down sampling factor

--- drivers/cpufreq/cpufreq_ondemand.c
+++ drivers/cpufreq/cpufreq_ondemand.c
@@ -22,6 +22,7 @@
 #include <linux/tick.h>
 #include <linux/ktime.h>
 #include <linux/sched.h>
+#include <linux/zentune.h>
 
 /*
  * dbs is used in this file as a shortform for demandbased switching
@@ -30,7 +31,13 @@
 
 #define DEF_FREQUENCY_DOWN_DIFFERENTIAL		(26)
 #define DEF_FREQUENCY_UP_THRESHOLD		(63)
+
+#if defined(CONFIG_ZEN_DEFAULT)
 #define DEF_SAMPLING_DOWN_FACTOR		(1)
+#elif defined(CONFIG_ZEN_CUSTOM)
+#define DEF_SAMPLING_DOWN_FACTOR		(DEF_SAMPLING_DOWN_FACTOR_CUSTOM)
+#endif
+
 #define MAX_SAMPLING_DOWN_FACTOR		(100000)
 #define MICRO_FREQUENCY_DOWN_DIFFERENTIAL	(3)
 #define MICRO_FREQUENCY_UP_THRESHOLD		(95)
