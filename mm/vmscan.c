1defe9c008f8e49355f3aed6981c9012221665ed
Evgeny Duzhakov <diabolo@diabolo-tm.ru>
2013-10-06 20:35:54 +0400

    Reiser4 for 3.11.1 patch applyed

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -2334,6 +2334,9 @@ static unsigned long do_try_to_free_pages(struct zonelist *zonelist,
 	struct zone *zone;
 	unsigned long writeback_threshold;
 	bool aborted_reclaim;
+	void *save = current->journal_info; /* save journal info */
+
+	current->journal_info = NULL;
 
 	delayacct_freepages_start();
 
@@ -2396,6 +2399,8 @@ static unsigned long do_try_to_free_pages(struct zonelist *zonelist,
 
 out:
 	delayacct_freepages_end();
+	/* restore journal info */
+	current->journal_info = save;
 
 	if (sc->nr_reclaimed)
 		return sc->nr_reclaimed;
