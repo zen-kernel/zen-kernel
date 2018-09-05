d8d095abe860a2e36fd3459ec3de92bf298f7c6a
Jan Alexander Steffens (heftig) <jan.steffens@gmail.com>
2018-09-05 12:42:44 +0200

    muqss: Make hrtimeout patches depend on MuQSS

--- kernel/time/hrtimer.c
+++ kernel/time/hrtimer.c
@@ -2217,6 +2217,7 @@ int __sched schedule_hrtimeout(ktime_t *expires,
 }
 EXPORT_SYMBOL_GPL(schedule_hrtimeout);
 
+#ifdef CONFIG_SCHED_MUQSS
 /*
  * As per schedule_hrtimeout but taskes a millisecond value and returns how
  * many milliseconds are left.
@@ -2326,3 +2327,4 @@ long __sched schedule_msec_hrtimeout_uninterruptible(long timeout)
 	return schedule_msec_hrtimeout(timeout);
 }
 EXPORT_SYMBOL(schedule_msec_hrtimeout_uninterruptible);
+#endif /* CONFIG_SCHED_MUQSS */
