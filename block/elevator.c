8028b431ef43dc6ffe5ade68e14f3ebb2a47589e
Jan Alexander Steffens (heftig) <jan.steffens@gmail.com>
2020-01-27 18:11:05 +0100

    ZEN: INTERACTIVE: Use BFQ as the elevator for SQ devices

--- block/elevator.c
+++ block/elevator.c
@@ -740,7 +740,11 @@ void elv_update_nr_hw_queues(struct request_queue *q, struct elevator_type *e,
 void elevator_set_default(struct request_queue *q)
 {
 	struct elv_change_ctx ctx = {
+#if defined(CONFIG_ZEN_INTERACTIVE) && defined(CONFIG_IOSCHED_BFQ)
+		.name = "bfq",
+#else
 		.name = "mq-deadline",
+#endif
 		.no_uevent = true,
 	};
 	int err;
