f32e5d5a5d5173a44b01098b0e6fad32073893d9
Steven Barrett <steven@liquorix.net>
2021-02-26 16:10:11 -0600

    Revert "Merge remote-tracking branch 'github/5.11/clearlinux' into HEAD"
    As time has gone on, we've received more questions about specific
    patches from clearlinux and their negative impact on Zen Kernel.  It
    doesn't seem like Intel completely knows what they're doing here and the
    fact none of these patches have gone upstream means they've lost
    interest or they have undocumented side-effects that are not good for
    most people (maybe especially AMD?).
    
    Remove ClearLinux patches and only bring back the only interesting
    patches piecemeal through our "zen-sauce" branch.
    
    This reverts commit 3f19c5b84d247004795a2bae04148db7f4ab3b69, reversing
    changes made to 81624ff802ca912a90574efa34bda5795ad96a0f.

--- kernel/locking/rwsem.c
+++ kernel/locking/rwsem.c
@@ -668,7 +668,6 @@ rwsem_spin_on_owner(struct rw_semaphore *sem)
 	struct task_struct *new, *owner;
 	unsigned long flags, new_flags;
 	enum owner_state state;
-	int i = 0;
 
 	owner = rwsem_owner_flags(sem, &flags);
 	state = rwsem_owner_state(owner, flags);
@@ -702,8 +701,7 @@ rwsem_spin_on_owner(struct rw_semaphore *sem)
 			break;
 		}
 
-		if (i++ > 1000)
-			cpu_relax();
+		cpu_relax();
 	}
 	rcu_read_unlock();
 
