f4e5954895b1f71719ad99f6ad9c70a4864d9708
Yu Zhao <yuzhao@google.com>
2021-04-05 04:17:41 -0600

    mm: multigenerational lru: mm_struct list
    To scan PTEs for accessed pages, a mm_struct list is maintained for
    each memcg. When multiple threads traverse the same memcg->mm_list,
    each of them gets a unique mm_struct and therefore they can run
    walk_page_range() concurrently to reach page tables of all processes
    of this memcg.
    
    And to skip page tables of processes that have been sleeping since the
    last walk, the usage of mm_struct is also tracked between context
    switches.
    
    Signed-off-by: Yu Zhao <yuzhao@google.com>
    Tested-by: Konstantin Kharlamov <Hi-Angel@yandex.ru>
    Change-Id: I25d9eda8c6bdc7c3653b9f210a159d6c247c81e8

--- include/linux/memcontrol.h
+++ include/linux/memcontrol.h
@@ -228,6 +228,8 @@ struct obj_cgroup {
 	};
 };
 
+struct lru_gen_mm_list;
+
 /*
  * The memory controller data structure. The memory controller controls both
  * page cache and RSS per cgroup. We would eventually like to provide
@@ -345,6 +347,10 @@ struct mem_cgroup {
 
 #ifdef CONFIG_TRANSPARENT_HUGEPAGE
 	struct deferred_split deferred_split_queue;
+#endif
+
+#ifdef CONFIG_LRU_GEN
+	struct lru_gen_mm_list *mm_list;
 #endif
 
 	struct mem_cgroup_per_node *nodeinfo[];
