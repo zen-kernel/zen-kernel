55a2edbcff4a06dbce281cfd12c69fbe959af37f
Alfred Chen <cchalpha@gmail.com>
2021-05-17 10:25:28 +0000

    sched/pds: PDS improvement.
    PDS uses bitmap queue as queue data structure. Rename maybe needed after
    all improvement are done.

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -45,7 +45,6 @@
 #include <linux/kcsan.h>
 #include <linux/rv.h>
 #include <linux/uidgid_types.h>
-#include <linux/skip_list.h>
 #include <linux/tracepoint-defs.h>
 #include <asm/kmap_size.h>
 
@@ -877,10 +876,8 @@ struct task_struct {
 #ifdef CONFIG_SCHED_PDS
 	u64				deadline;
 	u64				priodl;
-	/* skip list level */
-	int				sl_level;
-	/* skip list node */
-	struct skiplist_node		sl_node;
+	int				sq_idx;
+	struct list_head		sq_node;
 #endif /* CONFIG_SCHED_PDS */
 	/* sched_clock time spent running */
 	u64				sched_time;
