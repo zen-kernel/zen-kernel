4a3d23beb7cf00d624877a8f2eb4f7fb127211d6
Steven Barrett <steven@liquorix.net>
2021-08-11 18:41:50 -0500

    ZEN: Tune mgLRU to protect cache used in the last second
    Although not identical to the le9 patches that protect a byte-amount of
    cache through tunables, multigenerational LRU now supports protecting
    cache accessed in the last X milliseconds.
    
    In #218, Yu recommends starting with 1000ms and tuning as needed.  This
    looks like a safe default and turning on this feature should help users
    that don't know they need it.

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -4382,7 +4382,11 @@ static bool age_lruvec(struct lruvec *lruvec, struct scan_control *sc,
 }
 
 /* to protect the working set of the last N jiffies */
+#ifdef CONFIG_ZEN_INTERACTIVE
+static unsigned long lru_gen_min_ttl __read_mostly = HZ;
+#else
 static unsigned long lru_gen_min_ttl __read_mostly;
+#endif
 
 static void lru_gen_age_node(struct pglist_data *pgdat, struct scan_control *sc)
 {
