3eb61cbfa1c8d008b732074f2e598895bcae626c
Steven Barrett <steven@liquorix.net>
2021-09-01 21:30:55 -0500

    muqss: Port "sched: Make nr_iowait_cpu() return 32-bit value"

--- kernel/sched/MuQSS.c
+++ kernel/sched/MuQSS.c
@@ -3175,7 +3175,7 @@ unsigned long long nr_context_switches(void)
  * runnable.
  */
 
-unsigned long nr_iowait_cpu(int cpu)
+unsigned int nr_iowait_cpu(int cpu)
 {
 	return atomic_read(&cpu_rq(cpu)->nr_iowait);
 }
