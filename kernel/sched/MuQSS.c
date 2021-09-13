175580d5adeaaaef1939cb7763a95d86b062606c
Steven Barrett <steven@liquorix.net>
2021-09-12 20:29:39 -0500

    muqss: Sync "prepare_stask_switch" from mainline

--- kernel/sched/MuQSS.c
+++ kernel/sched/MuQSS.c
@@ -2820,6 +2820,7 @@ static inline void kmap_local_sched_in(void)
 /**
  * prepare_task_switch - prepare to switch tasks
  * @rq: the runqueue preparing to switch
+ * @prev: the current task that is being switched out
  * @next: the task we are going to switch to.
  *
  * This is called with the rq lock held and interrupts off. It must
