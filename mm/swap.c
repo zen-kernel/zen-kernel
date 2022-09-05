50b4e74fa21b1f90a0af037aa1b4978c4fe17a8e
Steven Barrett <steven@liquorix.net>
2022-09-05 11:35:20 -0500

    ZEN: mm/swap: Disable swap-in readahead
    Per an issue [1] on the chromium project, swap-in readahead causes more
    jank than not.  This might be caused by poor optimization on the
    swapping code, or the fact under memory pressure, we're pulling in pages
    we don't need, causing more swapping.
    
    Either way, this is mainline/upstream to Chromium, and ChromeOS
    developers care a lot about system responsiveness. Lets implement the
    same change so Zen Kernel users benefit.
    
    [1] https://bugs.chromium.org/p/chromium/issues/detail?id=263561

--- mm/swap.c
+++ mm/swap.c
@@ -1132,11 +1132,16 @@ void __init swap_setup(void)
 {
 	unsigned long megs = totalram_pages() >> (20 - PAGE_SHIFT);
 
+#ifdef CONFIG_ZEN_INTERACTIVE
+	/* Only swap-in pages requested, avoid readahead */
+	page_cluster = 0
+#else
 	/* Use a smaller cluster for small-memory machines */
 	if (megs < 16)
 		page_cluster = 2;
 	else
 		page_cluster = 3;
+#endif
 	/*
 	 * Right now other parts of the system means that we
 	 * _really_ don't want to cluster much more
