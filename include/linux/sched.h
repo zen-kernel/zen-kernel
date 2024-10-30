98dfee20f17db60624831ca133df70f0ef0a068c
Alfred Chen <cchalpha@gmail.com>
2024-10-30 14:53:40 +0000

    sched/alt: Simpifly migrate_enable/disable design.
    Simpifly Project C task migrate_enable/disable design by removing
    migration_flags and unused migration_pending in task structure.

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -910,11 +910,15 @@ struct task_struct {
 	const cpumask_t			*cpus_ptr;
 	cpumask_t			*user_cpus_ptr;
 	cpumask_t			cpus_mask;
+#ifndef CONFIG_SCHED_ALT
 	void				*migration_pending;
+#endif
 #ifdef CONFIG_SMP
 	unsigned short			migration_disabled;
 #endif
+#ifndef CONFIG_SCHED_ALT
 	unsigned short			migration_flags;
+#endif
 
 #ifdef CONFIG_PREEMPT_RCU
 	int				rcu_read_lock_nesting;
