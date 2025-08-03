08d11019f3924f192e1a68d7d2007f8ec7bdf889
Jan Alexander Steffens (heftig) <heftig@archlinux.org>
2025-08-03 15:27:26 +0200

    squash! ZEN: INTERACTIVE: Use Kyber as the elevator for MQ devices
    Fall back straight to none instead of mq-deadline. Some benchmarks in a
    [recent paper][1] suggest that mq-deadline has too much lock contention,
    hurting throughput and eating CPU waiting for spinlocks.
    
    [1]: https://research.spec.org/icpe_proceedings/2024/proceedings/p154.pdf

--- block/elevator.c
+++ block/elevator.c
@@ -740,8 +740,6 @@ void elevator_set_default(struct request_queue *q)
 	if (q->nr_hw_queues != 1 && !blk_mq_is_shared_tags(q->tag_set->flags))
 #if defined(CONFIG_ZEN_INTERACTIVE) && defined(CONFIG_MQ_IOSCHED_KYBER)
 		ctx.name = "kyber";
-#elif defined(CONFIG_ZEN_INTERACTIVE)
-		ctx.name = "mq-deadline";
 #else
 		return;
 #endif
