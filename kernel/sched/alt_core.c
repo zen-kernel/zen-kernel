a4f1c73983522e5a34fbd0504fa6e865ab453aa6
Alfred Chen <cchalpha@gmail.com>
2026-01-02 15:50:01 +0800

    sched/alt: [Sync] d245698d727a cgroup: Defer task cgroup unlink until after the task is done switching out

--- kernel/sched/alt_core.c
+++ kernel/sched/alt_core.c
@@ -3586,6 +3586,8 @@ static struct rq *finish_task_switch(struct task_struct *prev)
 		mmdrop_lazy_tlb_sched(mm);
 	}
 	if (unlikely(prev_state == TASK_DEAD)) {
+		cgroup_task_dead(prev);
+
 		/* Task is done with its stack. */
 		put_task_stack(prev);
 
