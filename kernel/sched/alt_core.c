af653103fe1ed882f6dca578b8b55b0b2484220c
Alfred Chen <cchalpha@gmail.com>
2026-03-02 10:45:04 +0800

    sched/alt: [Sync] 8d737320166b sched: Fix build for modules using set_tsk_need_resched()

--- kernel/sched/alt_core.c
+++ kernel/sched/alt_core.c
@@ -963,6 +963,7 @@ void __trace_set_need_resched(struct task_struct *curr, int tif)
 {
 	trace_sched_set_need_resched_tp(curr, smp_processor_id(), tif);
 }
+EXPORT_SYMBOL_GPL(__trace_set_need_resched);
 
 static inline void resched_curr(struct rq *rq)
 {
