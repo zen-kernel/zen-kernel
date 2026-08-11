df7b3eb5ad2e7ff403f4c32b1d2f2ec452e3ccde
Steven Barrett <steven@liquorix.net>
2026-08-11 17:18:40 -0500

    sched/alt: Drop dead check_task_changed()/balance_func remnants
    - check_task_changed() is declared in alt_core.h but has no definition
      or caller anywhere in the tree.
    - rq->balance_func and the balance_func_t typedef are written NULL once
      at init and never read; topology balancing dispatches through
      per_cpu(sched_cpu_topo_balance) instead.

--- kernel/sched/alt_sched.h
+++ kernel/sched/alt_sched.h
@@ -143,8 +143,6 @@ struct balance_callback {
 	void (*func)(struct rq *rq);
 };
 
-typedef void (*balance_func_t)(struct rq *rq, int cpu);
-
 struct balance_arg {
 	struct task_struct	*task;
 	int			active;
@@ -195,7 +193,6 @@ struct rq {
 	struct sched_avg	avg_irq;
 #endif
 
-	balance_func_t		balance_func;
 	struct balance_arg	active_balance_arg		____cacheline_aligned;
 	struct cpu_stop_work	active_balance_work;
 
