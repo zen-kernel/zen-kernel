79439d34dea5108741f00eb4031e777150cc4606
Steven Barrett <steven@liquorix.net>
2026-09-04 19:51:14 -0500

    Revert "sched/alt: Put a sync wakee on the waker's sibling before its idle CPU"
    This reverts commit c8852d48d1f50adbcf97c954b346703a29a9a90c.

--- kernel/sched/alt_core.c
+++ kernel/sched/alt_core.c
@@ -2087,17 +2087,16 @@ static inline int select_task_rq(struct task_struct *p, int wake_flags)
 	}
 
 	if (want_affine) {
-		int affine_cpu = nr_cpu_ids;
+		int affine_cpu = wake_affine_idle(cpu, prev_cpu, sync);
 
-		if (!is_idle_task(current)) {
+		if (affine_cpu == cpu && cpumask_test_cpu(cpu, &allow_mask)) {
+			int i;
+
 			cpumask_and(&mask, cpu_smt_mask(cpu), &allow_mask);
-			affine_cpu = cpumask_any_but(&mask, cpu);
+			i = cpumask_any_but(&mask, cpu);
+			new_cpu = i < nr_cpu_ids ? i : cpu;
+			goto out;
 		}
-		if (affine_cpu >= nr_cpu_ids)
-			affine_cpu = wake_affine_idle(cpu, prev_cpu, sync);
-		else if (!available_idle_cpu(affine_cpu) &&
-			 cpumask_intersects(&allow_mask, sched_idle_mask))
-			affine_cpu = nr_cpu_ids;
 		if (affine_cpu < nr_cpu_ids &&
 		    cpumask_test_cpu(affine_cpu, &allow_mask)) {
 			new_cpu = affine_cpu;
